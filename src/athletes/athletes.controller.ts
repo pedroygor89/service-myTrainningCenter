ff73e73544f4e1477fa44da328e93fe5932e01f5
Pedro Ygor <pedroygor.di@gmail.com>
2024-04-09 22:40:14 -0300

    Controller Ident

--- src/athletes/athletes.controller.ts
+++ src/athletes/athletes.controller.ts
@@ -1,20 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { CreateAthleteDto } from './dto/createAthletes.dto';
 import { AthletesService } from './athletes.service';
 import { Athlete } from './interfaces/athletes.interface';
 
 @Controller('api/v1/athletes')
 export class AthletesController {
-    constructor(private readonly athletesService: AthletesService) {}
+  constructor(private readonly athletesService: AthletesService) {}
 
-    @Post()
-    async createUpdateAthlete(@Body() createAthleteDto: CreateAthleteDto) {
-      await this.athletesService.createAthlete(createAthleteDto);
-    }
-  
-    @Get()
-    async findAthlete(): Promise<Athlete[]> {
-      return this.athletesService.findAllAthlete();
-    }
+  @Post()
+  async createUpdateAthlete(@Body() createAthleteDto: CreateAthleteDto) {
+    await this.athletesService.createAthlete(createAthleteDto);
+  }
 
+  @Get()
+  async findAthlete(): Promise<Athlete[]> {
+    return this.athletesService.findAllAthlete();
+  }
 }
